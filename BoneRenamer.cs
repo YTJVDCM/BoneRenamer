2ebe78dc7fe244946b1811205d3bdec64a1784d1
Azukimochi <Azukimochi25565@gmail.com>
2025-07-19 20:39:28 +0900

    XMLの読み込み処理を見直し ・データ用のディレクトリ配下からXMLを読み込むように ・読み込んだXMLのディレクトリパスとアバターの名前を結合し、アバターの名称とするように

--- BoneRenamer.cs
+++ BoneRenamer.cs
@@ -7,6 +7,7 @@ using System.Xml.Linq;
 using AvaterInfo;
 using Unity.Collections;
 using System;
+using UnityEngine.UI;
 
 public class BoneRenamer : EditorWindow
 {
@@ -23,93 +24,104 @@ public class BoneRenamer : EditorWindow
     }
     private void OnGUI()
     {
-        xml = new XMLLoader();
-        GUILayout.MaxHeight(5);
-        GUILayout.Label("ボーン名を変更したいオブジェクトを指定");
+        if (xml == null)
+            xml = new XMLLoader();
 
+        GUILayout.Label("ボーン名を変更したいオブジェクトを指定");
         TargetObj = (GameObject)EditorGUILayout.ObjectField("GameObject", TargetObj, typeof(GameObject), true);
 
-        GUILayout.Label("");
-        string[] avaterNames = xml.GetAvaterNames();
-
-        GUILayout.Label("変更先のアバター名");
-        var index = avaterNames.Length > 0 ? EditorGUILayout.Popup(SelectIndex, avaterNames) : -1;
-
-        if (index != SelectIndex)
+        if (GUILayout.Button("アバター辞書を再読み込み"))
         {
-            SelectIndex = index;
+            xml.LoadXML();
+            ResultMessage = "再読み込み完了";
+            Debug.Log("再読み込み完了");
         }
+
+        GUILayout.Space(5);
+        string[] avaterNames = xml.GetAvaterNames();
+        GUILayout.Label("変更先のアバター名");
+
+        if (avaterNames.Length > 0)
+        {
+            SelectIndex = EditorGUILayout.Popup(SelectIndex, avaterNames);
+        }
+
         SelectUseStartWith = GUILayout.Toggle(SelectUseStartWith, "先頭一致でリネーム");
+
         if (GUILayout.Button("適用"))
         {
             int matchCount = 0;
             if (TargetObj == null)
+            {
                 ResultMessage = "Objectが設定されていません";
+            }
+            else if (avaterNames.Length == 0)
+            {
+                ResultMessage = "アバター名が読み込まれていません";
+            }
             else
-                if (BoneRenameProcess(TargetObj, ref matchCount))
+            {
+                if (BoneRenameProcess(TargetObj, avaterNames[SelectIndex], ref matchCount))
                 {
-                    ResultMessage = "正常に処理が終了しました ボーン置換数:" + matchCount;
-                    Debug.Log("正常終了,ボーン置換数：" + matchCount);
+                    ResultMessage = $"正常に処理が終了しました ボーン置換数: {matchCount}";
+                    Debug.Log(ResultMessage);
                 }
-               
+            }
         }
+
         GUILayout.Label(ResultMessage);
     }
-    private static bool BoneRenameProcess (GameObject obj, ref int matchCount)
+    private static bool BoneRenameProcess(GameObject obj, string avatarKey, ref int matchCount)
     {
-        Transform children = obj.GetComponentInChildren<Transform>();
-        
-        if (children.childCount == 0) {
+        var boneMap = xml.GetBoneMapByAvatarName(avatarKey);
+        if (boneMap == null)
+        {
+            Debug.LogError("選択されたアバターに対応するボーンデータが見つかりません: " + avatarKey);
             return false;
         }
+
+        Transform children = obj.GetComponentInChildren<Transform>();
+        if (children.childCount == 0)
+        {
+            return false;
+        }
+
         foreach (Transform ob in children)
         {
             string tName = ob.gameObject.name;
-            foreach (XMLLoader.keys key in Enum.GetValues(typeof(XMLLoader.keys))) 
-            {
-                string[] bones = xml.GetBoneNameFromKey(key);
 
-                //先頭一致非使用
-                if(! SelectUseStartWith)
+            foreach (var kv in boneMap)
+            {
+                string boneKey = kv.Key;
+                string boneName = kv.Value;
+
+                if (!SelectUseStartWith)
                 {
-                    for (int i = 0; i < bones.Length; i++)
+                    if (tName == boneName)
                     {
-                        //Debug.Log("検索:" +tName + "/ "+ bones[i]);
-                        if (tName == bones[i])
-                        {
-                            Debug.Log("置換:" + tName + " -> " + bones[SelectIndex]);
-                            ob.gameObject.name = bones[SelectIndex];
-                            matchCount++;
-                            break;
-                        }
+                        Debug.Log($"置換: {tName} → {boneName}");
+                        ob.gameObject.name = boneName;
+                        matchCount++;
+                        break;
                     }
                 }
-                //先頭一致使用
                 else
                 {
-                    string rep = "";
-                    for (int i = 0; i < bones.Length; i++)
+                    if (tName.StartsWith(boneName) && tName != boneName)
                     {
-                        if (tName.Equals(bones[SelectIndex]))
-                            continue;
-                        //Debug.Log("検索:" +tName + "/ "+ bones[i]);
-                        if (tName.StartsWith(bones[i]))
-                        {
-                            //Debug.Log("合致:" + tName + " / " + bones[i]);
-                            if (rep.Length <= bones[i].Length)  
-                                rep = bones[i];
-                        }
-                    }
-                    if(!rep.Equals(""))
-                    {
-                        Debug.Log("置換：" + tName+ " : " + rep + " -> " + bones[SelectIndex]);
+                        Debug.Log($"置換: {tName}（StartsWith: {boneName}）→ {boneName + tName.Substring(boneName.Length)}");
+                        ob.gameObject.name = boneName + tName.Substring(boneName.Length);
                         matchCount++;
-                        ob.gameObject.name = tName.Replace(rep, bones[SelectIndex]);
+                        break;
                     }
                 }
             }
-            BoneRenameProcess(ob.gameObject, ref matchCount);
+
+            // 再帰処理
+            BoneRenameProcess(ob.gameObject, avatarKey, ref matchCount);
         }
+
         return true;
     }
+
 }
